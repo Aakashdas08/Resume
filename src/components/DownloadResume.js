920fe1f81630190d884829c49fd68df8752b5140
Aakash Das <aakashdas08@gmail.com>
2021-08-12 20:45:52 +0530

    add pdf file

--- src/components/DownloadResume.js
+++ src/components/DownloadResume.js
@@ -4,7 +4,7 @@ const DownloadResume = () => {
 
   return (
     <a className="resume-download" title="download pdf"
-      download="aashish-resume" href="/aashish-resume.pdf">
+      download="aakash-das" href="/aakash-das-vercel-app.pdf">
       <i className="far fa-file-pdf" />
     </a>
   )
